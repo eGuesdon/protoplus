2348850778f5eb770f484e248e4d514077864df9
parlangeu <ugo.parlange@cgi.com>
2017-02-09 18:38:55 +0100

    Initialisation Angular 2

--- src/app/components/root/source.ts
+++ src/app/components/root/source.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'body',
   templateUrl: './template.html',
-  styleUrls: ['./style.css']
+  styleUrls: ['./styles.css']
 })
 export class AppComponent {
   title = 'app works!';
