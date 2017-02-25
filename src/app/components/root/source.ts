0934439b50c66c89187fbbe2ca10ec68b0c105ea
Eric Guesdon <cahe@macbook-pro-de-eric.home>
2017-02-25 19:52:18 +0100

    Changes

--- src/app/components/root/source.ts
+++ src/app/components/root/source.ts
@@ -19,14 +19,16 @@ export class RootComponent {
   public ngOnInit () : void {
     // Create an empty menubar
     this.menu = new nw.Menu({type:"menubar"});
+    // Needs to be define for MacOS
     this.menu.createMacBuiltin("Proto+", { hideWindow : true , hideEdit : true } ) ;
 
     // Create a submenu as the 2nd level menu
     var submenu : nw.Menu = new nw.Menu();
     submenu.append(new nw.MenuItem({ label: 'Eric' }));
     submenu.append(new nw.MenuItem({ label: 'Corinne' }));
-    submenu.append(new nw.MenuItem({ label: 'Agnès' }));
-    submenu.append(new nw.MenuItem({ label: 'Hina' }));
+    submenu.append(new nw.MenuItem({ type: 'Separator' }));
+    submenu.append(new nw.MenuItem({ label: 'Ugo' }));
+    submenu.append(new nw.MenuItem({ label: 'Noelle' }));
 
     // Create and append the 1st level menu to the menubar
     this.menu.append(new nw.MenuItem({
@@ -36,7 +38,7 @@ export class RootComponent {
     nw.Window.get().menu = this.menu;
     nw.Window.get().moveBy(0, -200);
     nw.Window.get().showDevTools();
-    console.log(nw.Window.get()) ;
+    console.log(nw.Window.get) ;
   }
   
   // ==== Getter / Setter ==== \\
