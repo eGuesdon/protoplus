07b05b16541c1554ef9d3dd8ef8ea43931e0afd8
Eric Guesdon <cahe@macbook-pro-de-eric.home>
2017-02-25 11:46:21 +0100

    test and typings installation

--- src/app/components/root/source.ts
+++ src/app/components/root/source.ts
@@ -9,26 +9,17 @@ declare let nw: any;
 export class RootComponent {
   welcomeMsg : string = 'Bienvenu sur Proto';
   welcomeMsgPlus : string = '+' ;
+  menu : Menu;
 
   constructor() {
     
   }
 
-  ngOnInit() {
-    const menu = new nw.Menu({type: 'menubar'});
+  public ngOnInit() {
+    this.createMenu();
+  }
 
-    // Create a submenu as the 2nd level menu
-    const submenu = new nw.Menu();
-    submenu.append(new nw.MenuItem({ label: 'Item A' }));
-    submenu.append(new nw.MenuItem({ label: 'Item B' }));
+  private createMenu():void {
 
-    // Create and append the 1st level menu to the menubar
-    menu.append(new nw.MenuItem({
-      label: 'First Menu',
-      submenu: submenu
-    }));
-
-    // Assign it to `window.menu` to get the menu displayed
-    nw.Window.get().menu = menu;
   }
 }
