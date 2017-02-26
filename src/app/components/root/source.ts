9e12f4dc1056dee1d862e664a2d9971a6b920995
parlangeu <ugo.parlange@cgi.com>
2017-02-26 16:21:29 +0100

    Optimisation debug Montée de version des dépendances

--- src/app/components/root/source.ts
+++ src/app/components/root/source.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../../../typings/globals/nw.js/index.d.ts" />
-
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,63 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./styles.css']
 })
 export class RootComponent {
-  public welcomeMsg : string = 'Bienvenu sur Proto';
-  public welcomeMsgPlus : string = '+' ;
-  private _menu : nw.Menu;
+  public welcomeMsg: string = 'Bienvenu sur Proto';
+  public welcomeMsgPlus: string = '+';
+  private _menu: nw.Menu;
 
   constructor() {
-    
+
   }
 
-  public ngOnInit () : void {
+  public ngOnInit(): void {
     // Create an empty menubar
-    this.menu = new nw.Menu({type:"menubar"});
+    this.menu = new nw.Menu({ type: "menubar" });
     // Needs to be define for MacOS
     // add - { hideWindow : true , hideEdit : true } - to remove Edit and Window menu entries
     // how to replace nwjs and replace it by the my-app-name : have a look at http://docs.nwjs.io/en/latest/For%20Users/Advanced/Customize%20Menubar/#mac-os-x
-    this.menu.createMacBuiltin("Proto+" ) ;
+    if (process.platform === "darwin") {
+      this.menu.createMacBuiltin("Proto+");
+    }
 
     // Create a submenu as the 2nd level menu
-    var submenu : nw.Menu = new nw.Menu();
-    
-    var ssubmenu : nw.Menu = new nw.Menu();
+    var submenu: nw.Menu = new nw.Menu();
+
+    var ssubmenu: nw.Menu = new nw.Menu();
     ssubmenu.append(new nw.MenuItem({ label: 'Noelle' }));
     ssubmenu.append(new nw.MenuItem({ label: 'Ugo' }));
 
-    submenu.append(new nw.MenuItem({ 
-        label: 'Famille Parlange',
-        submenu:ssubmenu
+    submenu.append(new nw.MenuItem({
+      label: 'Famille Parlange',
+      submenu: ssubmenu
     }));
 
     submenu.append(new nw.MenuItem({ type: 'separator' }));
-    
-    var sssubmenu : nw.Menu = new nw.Menu();
+
+    var sssubmenu: nw.Menu = new nw.Menu();
     sssubmenu.append(new nw.MenuItem({ label: 'Corinne' }));
     sssubmenu.append(new nw.MenuItem({ label: 'Eric' }));
 
-    submenu.append(new nw.MenuItem({ 
-        label: 'Famille Guesdon',
-        submenu:sssubmenu
+    submenu.append(new nw.MenuItem({
+      label: 'Famille Guesdon',
+      submenu: sssubmenu
     }));
 
     // Create and append the 1st level menu to the menubar
     this.menu.append(new nw.MenuItem({
-        label: 'Famille(s)',
-        submenu: submenu
-      }));
-    nw.Window.get().menu = this.menu ;
-    nw.Window.get().moveBy(0, -200) ;
-    nw.Window.get().showDevTools() ;
+      label: 'Famille(s)',
+      submenu: submenu
+    }));
+    nw.Window.get().menu = this.menu;
+    nw.Window.get().moveBy(0, -200);
+    nw.Window.get().showDevTools();
   }
-  
+
   // ==== Getter / Setter ==== \\
 
-  private set menu (m : nw.Menu) {
-    this._menu = m ;
+  private set menu(m: nw.Menu) {
+    this._menu = m;
   }
 
-  private get menu () : nw.Menu {
-    return this._menu ;
+  private get menu(): nw.Menu {
+    return this._menu;
   }
-  
+
 }
